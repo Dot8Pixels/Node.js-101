fe3d4de99dfeee7051fa72a785cbd3778c37020c
Napat Sri-ampun <napatsriz@gmail.com>
2022-04-14 10:26:21 +0700

    Add simple routing

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    const my_html = `
-    <h1>Hello Node.js</h1>
-    <p style="background:red">Dot8Pixels | 2022</p>
-    `
-
-    res.write(my_html);
-    res.end();
+    const pathName = req.url;
+    console.log("url = ", pathName)
+    if(pathName === "/" || pathName === "/home"){
+        res.end("<h1>Hello Homepage</h1>")
+    } else if(pathName === "/product"){
+        res.end("<h1>Hello Product</h1>")
+    } else{
+        res.writeHead(404)
+        res.end("<h1>Not Found</h1>")
+    }
+    
   })
   
   server.listen(8080, 'localhost', () => {
