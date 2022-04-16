2a11faaaead79845b8bff9cd332b13b1d9182c3b
Napat Sri-ampun <napatsriz@gmail.com>
2022-04-16 10:22:42 +0700

    Add Query String

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const http = require("http");
 const fs = require('fs')
+const url = require('url')
 
 const indexPage = fs.readFileSync(`${__dirname}/templates/index.html`, 'utf-8')
-const productPage = fs.readFileSync(`${__dirname}/templates/product1.html`, 'utf-8')
+const productPage1 = fs.readFileSync(`${__dirname}/templates/product1.html`, 'utf-8')
+const productPage2 = fs.readFileSync(`${__dirname}/templates/product2.html`, 'utf-8')
+const productPage3 = fs.readFileSync(`${__dirname}/templates/product3.html`, 'utf-8')
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
-    console.log("url = ", pathName)
-    console.log("dirname = ", __dirname)
-    if(pathName === "/" || pathName === "/home"){
+    
+    const {pathname, query} = url.parse(req.url, true);
+    if(pathname === "/" || pathname === "/home"){
         res.end(indexPage)
-    } else if(pathName === "/product"){
-        res.end(productPage)
+    } else if(pathname === "/product"){
+        if(query.id === "1"){
+            res.end(productPage1)
+        } else if(query.id === "2"){
+            res.end(productPage2)
+        } else if(query.id === "3"){
+            res.end(productPage3)
+        } else{
+            res.writeHead(404)
+            res.end("<h1>Not Found</h1>")
+        }
     } else{
         res.writeHead(404)
         res.end("<h1>Not Found</h1>")
