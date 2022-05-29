72134918ee2b4a0f6589f8e766c2ed51fa32b968
Napat Sri-ampun <napatsriz@gmail.com>
2022-05-29 09:33:30 +0700

    Add Views & Template engine

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -1,6 +1,9 @@
 // Manage Routing
 const express = require('express')
 const router = express.Router()
-const path = require('path')
+
+router.get('/', (req, res) => {
+    res.render('index.ejs')
+})
 
 module.exports = router
