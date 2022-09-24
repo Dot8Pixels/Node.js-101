a7c5aecede69c0b53a3f5f961f03b171557a6eb4
Napat Sri-ampun <napatsriz@gmail.com>
2022-09-24 07:53:00 +0700

    Add control flow if-else

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     const name = "DotPixels"
-    const age = 10
+    const age = 17
     const address = "<h3>Bangkok</h3>"
     res.render('index', {name:name, age:age, address:address})
 })
