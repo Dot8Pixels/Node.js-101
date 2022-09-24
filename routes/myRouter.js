6d564ef2ff7b6baa521833c0d14dd49e189a38a4
Napat Sri-ampun <napatsriz@gmail.com>
2022-09-24 07:49:08 +0700

    Pass values to template

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -3,7 +3,10 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('index.ejs')
+    const name = "DotPixels"
+    const age = 10
+    const address = "<h3>Bangkok</h3>"
+    res.render('index', {name:name, age:age, address:address})
 })
 
 module.exports = router
